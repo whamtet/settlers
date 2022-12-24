9bd4c8d74517a8048275ba14ab591c828f83f81a
Matthew Molloy <whamtet@gmail.com>
2022-12-24 17:04:47 +0900

    send retry

--- src/clj/ctmx/catan/sse.clj
+++ src/clj/ctmx/catan/sse.clj
@@ -1,5 +1,8 @@
 (ns ctmx.catan.sse
     (:require
+      [clojure.set :as set]
+      [ctmx.catan.state :as state]
+      [ctmx.render :as render]
       [ring.adapter.undertow.websocket :as ws]))
 
 (defn- dissoc-in [m ks]
@@ -14,3 +17,16 @@
 (defn remove-connection [game-name color]
   (fn [_]
     (swap! connections dissoc-in [game-name color])))
+
+(defn- send-retry
+  ([game-name e]
+   (future (send-retry game-name e state/valid-color? 19)))
+  ([game-name e recipients retries]
+   (let [available (get @connections game-name)
+         leftovers (set/difference recipients (set (keys available)))]
+     (doseq [[user connection] available :when (recipients user)]
+       (ws/send connection e false))
+     (when (and (pos? retries) (not-empty leftovers))))))
+
+(defn send! [game-name html]
+  (send-retry game-name (render/html html)))
