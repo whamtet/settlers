c88cf9cdf607d02245f669c3149aab0449bfb682
Matthew Molloy <whamtet@gmail.com>
2023-01-10 11:45:50 +1300

    update title!

--- src/clj/ctmx/catan/web/htmx.clj
+++ src/clj/ctmx/catan/web/htmx.clj
@@ -20,7 +20,7 @@
    [:head
     [:meta {:charset "UTF-8"}]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
-    [:title "Htmx + Kit"]
+    [:title "Settlers of Catan"]
     [:link {:rel "stylesheet"
             :href "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
             :integrity "sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
